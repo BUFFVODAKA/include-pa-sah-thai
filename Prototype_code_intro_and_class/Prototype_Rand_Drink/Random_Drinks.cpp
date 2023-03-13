d8c2b56b45c0f8d9e5b178c47c08d9849063ae6e
Tanapat Choeichomsri <tanapat_choeichomsri@cmu.ac.th>
2023-03-13 17:40:45 +0700

    เปลี่ยนภาษาไทยเป็น Eng และเพิ่ม Choice

--- Prototype_code_intro_and_class/Prototype_Rand_Drink/Random_Drinks.cpp
+++ Prototype_code_intro_and_class/Prototype_Rand_Drink/Random_Drinks.cpp
@@ -11,17 +11,31 @@ void GetDrinkList(vector<string> &DrinksLists,string FileNames){
     ifstream source;
     source.open(FileNames.c_str());
     string DrinksList;
-    char format[] = "%s";
     while(getline(source,DrinksList)){
-        sscanf(DrinksList.c_str(),format,DrinksList);
         DrinksLists.push_back(DrinksList);
     }
 }
 
 int main(){
+    ifstream source;
     string FileNames = "Drinks_List.txt";
     vector<string> DrinksLists;
     GetDrinkList(DrinksLists,FileNames);
-    int RNG = rand()%(DrinksLists.size()+1);
-    cout << "เราขอแนะนำให้ท่านดื่ม" << DrinksLists[RNG];
+    int Choice;
+    do{
+        cout << "Please Select (0)Stop Asking (1)See The List of Drinks (2)Suggest Drinks for you\n";
+        cin >> Choice;
+        if(Choice == 0){
+            break;
+        }else if(Choice == 1){
+            for(int i = 0; i < DrinksLists.size(); i++){
+                cout << "(" << i+1 << ")" << DrinksLists[i] << "\n";
+            }
+        }else if(Choice == 2){
+            int RNG = rand()%(DrinksLists.size()+1);
+            cout << "We Suggest you to drink " << DrinksLists[RNG] << "\n";
+        }else{
+            cout << "Invalid Input" << "\n";
+        }
+    }while(Choice != 0);
 }

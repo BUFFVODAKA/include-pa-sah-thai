82e94735b5d93767f39ec144e5047f5024ecc0b5
BUFFVODAKA <peppy16001@gmail.com>
2023-03-15 18:03:14 +0700

    codebehindmor

---
+++ prototype_code3/TalkCodeV1.01.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void main_talk_bot(string name)
+{
+    cout << "Enter talk_with_me (AI working line 9 and 100)ตอนนี้ยังไม่ได้ทำ" << endl; //เดี๋ยวค่อยทำ
+}
+
+int main(){
+    main_talk_bot();
+}

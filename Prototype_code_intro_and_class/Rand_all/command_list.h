ea950157a863d855041fd7edd80a1d688fffa8fd
SKOT <skotskip@gmail.com>
2023-03-16 17:51:06 +0700

    add_rand_all

---
+++ Prototype_code_intro_and_class/Rand_all/command_list.h
@@ -0,0 +1,84 @@
+//#include <iostream>
+//#include <string>
+//#include <vector>
+// #include <Windows.h>
+// #include <cstdlib>
+// #include <ctime>
+//#include <conio.h>
+
+
+using namespace std;
+
+namespace my_command
+{
+    void show_command(vector <string> command)
+    {
+        for(int i  = 0; i<command.size(); i++)
+        {
+            cout << command[i] << endl;
+        }    
+        cout << "ให้ฉํนทำอะไรดี : ";
+    }
+
+    void cout_string_dalay(string say)
+    {
+        for(int i = 0; i<say.size();i++)
+        {
+            cout <<say[i];
+            Sleep(3);
+        }
+        cout << endl;
+    }
+
+    void cout_vector_string_delay(vector<string> say)
+    {
+        for (int j =0; j < say.size(); j++)
+        {
+            for(int i = 0; i<say[j].size();i++)
+            {
+                cout <<say[j][i];
+                Sleep(3);
+            }
+            cout << endl;
+        }
+    }
+
+
+    void cout_string_delay_not_endl(string say)
+    {
+        for(int i = 0; i<say.size();i++)
+        {
+            cout <<say[i];
+            Sleep(3);
+        }
+
+        // for(char for_cout: say)
+        // {
+        //     cout << for_cout;
+        //     Sleep(3);
+        // }
+    }
+
+    void cout_delay_vector_and_wait(vector <string> say)
+    {
+        for (int j =0; j < say.size(); j++)
+        {
+            for(int i = 0; i<say[j].size();i++)
+            {
+                cout <<say[j][i];
+                Sleep(3);
+            }
+            cout << endl;
+            Sleep(1000);
+        }
+    }
+
+    void showcommand_and_line(vector <string> say)
+    {
+        for(string i: say)
+        {
+            cout << i << endl;
+        }
+        cout << say[0] << endl;
+    }
+}

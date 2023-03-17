cf3febc4a03cca5244fd75f7cff9b925e55638da
SKOT <skotskip@gmail.com>
2023-03-17 12:33:34 +0700

    แก้ Intro แล้ว

--- OUR_CHAT_BOT/Intro.cpp
+++ OUR_CHAT_BOT/Intro.cpp
@@ -12,80 +12,63 @@
 
 using namespace std;
 
-int say_in_intro;
-
 void give_name(string &);
 
 void Nono(string &name)
 {
     system("cls"); //ใช้เคลียร์ terminal
     cout << "y and n only" << endl;
-    //give_name(name);
 }
 
-void cout_delay_cuz_vector_bug(string name)
-{
-    vector <string> _say = {"ชื่อ Chat bot นี้ถูกตั้งเป็นค่าเริ่มต้น ("+name+") เรียบร้อยแล้ว","กรุณาใส่ชื่อที่คุณต้องการ (ถ้าไม่ใส่อะไรเลยชื่อจะกลายเป็นชื่อเริ่มต้น (" +name+") และ Enter เฉยๆ ก็ถือว่าใส่ชื่อแล้วนะ) ถ้าเป็นภาษาไทยหรือภาษาอื่นมัน cin ไม่ได้นะ"};
-
-    if(say_in_intro)
-    {
-        my_command::cout_string_dalay(_say[say_in_intro]);
-    }
-    else 
-    {
-        my_command::cout_string_dalay(_say[say_in_intro]);
-    }
-
-}
 void give_name(string &name)
 {
     string say = "คุณต้องการตั้งชื่อให้ chat bot ของเราหรือไม่ (y/n)";
-    //vector <string> _say = {"ชื่อ Chat bot นี้ถูกตั้งเป็นค่าเริ่มต้น ("+name+") เรียบร้อยแล้ว","กรุณาใส่ชื่อที่คุณต้องการ (ถ้าไม่ใส่อะไรเลยชื่อจะกลายเป็นชื่อเริ่มต้น (" +name+") และ Enter เฉยๆ ก็ถือว่าใส่ชื่อแล้วนะ) ถ้าเป็นภาษาไทยหรือภาษาอื่นมัน cin ไม่ได้นะ"};
+    vector <string> _say = {"ชื่อ Chat bot นี้ถูกตั้งเป็นค่าเริ่มต้น ("+name+") เรียบร้อยแล้ว","กรุณาใส่ชื่อที่คุณต้องการ (ถ้าไม่ใส่อะไรเลยชื่อจะกลายเป็นชื่อเริ่มต้น (" +name+") และ Enter เฉยๆ ก็ถือว่าใส่ชื่อแล้วนะ) ถ้าเป็นภาษาไทยหรือภาษาอื่นมัน cin ไม่ได้นะ","ชื่อ Chat bot นี้ถูกตั้งเป็น "+name+" เรียบร้อยแล้ว"};
     my_command::cout_string_dalay(say);
 
     bool flag = false;
 
     do
     {
-        char ans[3]; //แค่จองเผื่อ
+        string ans;
 
         cin >> ans;
         cin.clear(); //สำหรับเคลียร์ตัวที่เหลือ
         fflush(stdin); //ใช้กับ clear
 
-        if (ans[1] == '\0')
+        if (ans.size() > 1)
         {
+            system("cls"); //ใช้เคลียร์ terminal
+            cout << "y and n only" << endl;
+        }
+        else
+        {
+            char _ans = ans[0];
+
             switch (ans[0])
             {
                 case 'y':case 'Y':
-                    //my_command::cout_string_dalay(_say[1]); //bug
-                    say_in_intro = 1;
+                    my_command::cout_string_dalay(_say[1]); 
                     getline(cin, name);
-                    say = "ชื่อ Chat bot นี้ถูกตั้งเป็น "+name+" เรียบร้อยแล้ว";
-                    my_command::cout_string_dalay(say);
+                    my_command::cout_string_dalay(_say[2]);
                     flag = true;
                     Sleep(1000);
                     break;
                 case 'n':case 'N':
-                    say_in_intro = 0;
-                    //my_command::cout_string_dalay(_say[0]); //bug
+                    my_command::cout_string_dalay(_say[0]); 
                     Sleep(1000);
                     flag = true;
                     break;
                 default:
+                    system("cls"); //ใช้เคลียร์ terminal
                     Nono(name);
                     break;
             }
         }
-        else 
-        {
-            Nono(name);
-        }
-
     }
     while(flag != true);
 
-
+    cout << "exit while" << endl; //debug
 }
 
 int main()
@@ -95,6 +78,7 @@ int main()
     string name = "อับดุล"; // เปลี่ยนชื่อได้นะ
 
     give_name(name);
+    cout << "end give name" << endl; //debug
     system("cls");
 
     active_ai_intro(name);
